764f2cee413f0594fb68d58e4ae75f47df345a37
Andreas <andreas.aqua@gmx.at>
2025-05-18 06:16:16 +0200

    Add files via upload

---
+++ Klick-Alarm/klickAlarm.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("button");
+
+        button.addEventListener("click", function () {
+            button.innerText = "Button wurde gedrückt!";
+            button.style.background = "orange";
+        });
